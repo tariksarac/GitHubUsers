10daae4d332a5015660c1032f6654891f5550273
Tarik Sarac <tarik.sarac@symphony.is>
2018-12-19 19:33:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React yeee
           </a>
         </header>
       </div>
